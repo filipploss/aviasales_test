ced438c0bd77f3f77a7596749158c15457218bbd
Filipp Loss <filipploss@MacBook-Pro-Filipp.local>
2020-04-28 20:21:53 +0300

    ticket card fonts

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -4,26 +4,43 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 // import { ReactComponent as Logo } from "http://pics.avs.io/110/36/S7.png";
 
-
+// TODO: по алфавиту свойства
 const useStyles = makeStyles((theme) => ({
   card: {
     background: "#FFFFFF",
     boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
     borderRadius: "5px",
     fontFamily: "Open Sans",
-  },
-  header: {
-    display: "flex",
-    justifyContent: "space-between",
-    color: "#2196F3",
     fontStyle: "normal",
     fontWeight: 600,
+  },
+  header: {
+    color: "#2196F3",
+    display: "flex",
+    justifyContent: "space-between",
     fontSize: "24px",
     lineHeight: "24px",
   },
   destination: {
     display: "flex",
     justifyContent: "space-between",
+  },
+  destinationHeading: {
+    fontSize: "12px",
+    lineHeight: "18px",
+    // display: flex;
+    // align-items: center;
+    letterSpacing: "0.5px",
+    textTransform: "uppercase",
+    color: "#A0B0B9",
+  },
+  destinationText: {
+    fontSize: "14px",
+    lineHeight: "21px",
+    color: "#4A4A4A",
+    // /* identical to box height, or 150% */
+    // display: flex;
+    // align-items: center;
   },
 }));
 
@@ -34,15 +51,37 @@ export default function Ticket() {
     <Box className={classes.card}>
       <Box className={classes.header}>
         <Box>{price.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")} Р</Box>
-        <Box><img src="http://pics.avs.io/110/36/S7.png" alt=''/></Box>
+        <Box>
+          <img src="http://pics.avs.io/110/36/S7.png" alt="" />
+        </Box>
       </Box>
       <Box className={classes.destination}>
-        <Box>13400 Р</Box>
-        <Box>S7 Airlines</Box>
+        <Box>
+          <Box className={classes.destinationHeading}>MOW – HKT</Box>
+          <Box className={classes.destinationText}>10:45 – 08:00</Box>
+        </Box>
+        <Box>
+          <Box className={classes.destinationHeading}>В пути</Box>
+          <Box className={classes.destinationText}>21ч 15м</Box>
+        </Box>
+        <Box>
+          <Box className={classes.destinationHeading}>2 пересадки</Box>
+          <Box className={classes.destinationText}>HKG, JNB</Box>
+        </Box>
       </Box>
       <Box className={classes.destination}>
-        <Box>13400 Р</Box>
-        <Box>S7 Airlines</Box>
+        <Box>
+          <Box className={classes.destinationHeading}>MOW – HKT</Box>
+          <Box className={classes.destinationText}>10:45 – 08:00</Box>
+        </Box>
+        <Box>
+          <Box className={classes.destinationHeading}>В пути</Box>
+          <Box className={classes.destinationText}>21ч 15м</Box>
+        </Box>
+        <Box>
+          <Box className={classes.destinationHeading}>2 пересадки</Box>
+          <Box className={classes.destinationText}>HKG, JNB</Box>
+        </Box>
       </Box>
     </Box>
   );

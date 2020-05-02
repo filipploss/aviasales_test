cd95f7c802f21cdc8760ef82d9cdd72d3ea17204
Filipp Loss <filipploss@MacBook-Pro-Filipp.local>
2020-05-02 21:44:24 +0300

    refactor ticketCard

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -53,69 +53,156 @@ function TicketCard(props) {
 
   let price;
   let carrierImageUrl;
+  let firstSegmentOrigin;
+  let firstSegmentDestination;
   let firstSegmentDepartureDateAndTime;
-  let secondSegmentDepartureDateAndTime;
   let firstSegmentdepartureTime;
   let firstSegmentDepartureHours;
   let firstSegmentdepartureMinutes;
   let firstSegmentdepartureTimestamp;
   let firstSegmentArrivalHours;
   let firstSegmentArrivalMinutes;
-  
+  let firstSegmentTravelHours;
+  let firstSegmentTravelMinutes;
+  let firstSegmentStops;
+  let firstSegmentStopsNames;
+
+  let secondSegmentStops;
+  let secondSegmentOrigin;
+  let secondSegmentDestination;
   let secondSegmentdepartureTime;
   let secondSegmentDepartureHours;
   let secondSegmentDepartureMinutes;
+  let secondSegmentDepartureDateAndTime;
   let secondSegmentdepartureTimestamp;
   let secondSegmentArrivalHours;
   let secondSegmentArrivalMinutes;
+  let secondSegmentTravelHours;
+  let secondSegmentTravelMinutes;
+  let secondSegmentStopsNames;
 
-
-  
   if (props.data.tickets) {
-//  TODO!!!! Departute and arrival time is made for Moscow
-    price = props.data.tickets[0].price.toString();
-    carrierImageUrl= `http://pics.avs.io/110/36/${props.data.tickets[0].carrier}.png`
+    //  TODO!!!! Departute and arrival time is made for Moscow
+    price = props.data.tickets[0].price
+      .toString()
+      .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ");
+    carrierImageUrl = `http://pics.avs.io/110/36/${props.data.tickets[0].carrier}.png`;
     console.log("date data: ", props.data.tickets[0].segments[0].date);
-    firstSegmentDepartureDateAndTime = new Date(props.data.tickets[0].segments[0].date);
+
+    firstSegmentOrigin = props.data.tickets[0].segments[0].origin;
+    firstSegmentDestination = props.data.tickets[0].segments[0].destination;
+
+    secondSegmentOrigin = props.data.tickets[0].segments[1].origin;
+    secondSegmentDestination = props.data.tickets[0].segments[1].destination;
+    firstSegmentDepartureDateAndTime = new Date(
+      props.data.tickets[0].segments[0].date
+    );
     console.log("departureDateAndTime: ", firstSegmentDepartureDateAndTime);
-    firstSegmentDepartureHours = firstSegmentDepartureDateAndTime.getHours();
-    firstSegmentdepartureMinutes = firstSegmentDepartureDateAndTime.getMinutes();
+    firstSegmentDepartureHours = String(
+      firstSegmentDepartureDateAndTime.getHours()
+    ).padStart(2, "0");
+    firstSegmentdepartureMinutes = String(
+      firstSegmentDepartureDateAndTime.getMinutes()
+    ).padStart(2, "0");
+
     firstSegmentdepartureTime = firstSegmentDepartureDateAndTime.getTime();
     console.log("departureTime: ", firstSegmentdepartureTime);
     console.log("duration: ", props.data.tickets[0].segments[0].duration);
     firstSegmentdepartureTimestamp = new Date(
-      firstSegmentdepartureTime + props.data.tickets[0].segments[0].duration * 60000
+      firstSegmentdepartureTime +
+        props.data.tickets[0].segments[0].duration * 60000
     );
     console.log("timestamp + duration: ", firstSegmentdepartureTimestamp);
     console.log(firstSegmentdepartureTimestamp.getHours());
     console.log(firstSegmentdepartureTimestamp.getMinutes());
-    firstSegmentArrivalHours = firstSegmentdepartureTimestamp.getHours() + 7;
-    firstSegmentArrivalMinutes = firstSegmentdepartureTimestamp.getMinutes();
 
-    secondSegmentDepartureDateAndTime = new Date(props.data.tickets[0].segments[1].date); 
-    secondSegmentDepartureHours = secondSegmentDepartureDateAndTime.getHours();
-    secondSegmentDepartureMinutes = secondSegmentDepartureDateAndTime.getMinutes();
+    firstSegmentArrivalHours = String(
+      firstSegmentdepartureTimestamp.getHours()
+    ).padStart(2, "0");
+
+    firstSegmentArrivalMinutes = String(
+      firstSegmentdepartureTimestamp.getMinutes()
+    ).padStart(2, "0");
+
+    firstSegmentTravelHours = String(
+      Math.floor(props.data.tickets[0].segments[0].duration / 60)
+    ).padStart(2, "0");
+
+    firstSegmentTravelMinutes = String(
+      props.data.tickets[0].segments[0].duration -
+        Math.floor(props.data.tickets[0].segments[0].duration / 60) * 60
+    ).padStart(2, "0");
+
+    firstSegmentStops =
+      props.data.tickets[0].segments[0].stops.length === 0
+        ? "БЕЗ ПЕРЕСАДОК"
+        : props.data.tickets[0].segments[0].stops.length === 1
+        ? "1 ПЕРЕСАДКА"
+        : props.data.tickets[0].segments[0].stops.length > 1
+        ? `${props.data.tickets[0].segments[0].stops.length} ПЕРЕСАДКИ`
+        : "5 ПЕРЕСАДОК";
+
+      firstSegmentStopsNames = props.data.tickets[0].segments[0].stops.length > 1
+      ? props.data.tickets[0].segments[0].stops.join(", ")
+      : props.data.tickets[0].segments[0].stops.join("")
+
+    secondSegmentDepartureDateAndTime = new Date(
+      props.data.tickets[0].segments[1].date
+    );
+
+    secondSegmentDepartureHours = String(
+      secondSegmentDepartureDateAndTime.getHours()
+    ).padStart(2, "0");
+
+    secondSegmentDepartureMinutes = String(
+      secondSegmentDepartureDateAndTime.getMinutes()
+    ).padStart(2, "0");
     secondSegmentdepartureTime = secondSegmentDepartureDateAndTime.getTime();
     console.log("departureTime: ", secondSegmentdepartureTime);
     console.log("duration: ", props.data.tickets[0].segments[1].duration);
     secondSegmentdepartureTimestamp = new Date(
-      secondSegmentdepartureTime + props.data.tickets[0].segments[1].duration * 60000
+      secondSegmentdepartureTime +
+        props.data.tickets[0].segments[1].duration * 60000
     );
     console.log("timestamp + duration: ", secondSegmentdepartureTimestamp);
     console.log(secondSegmentdepartureTimestamp.getHours());
     console.log(secondSegmentdepartureTimestamp.getMinutes());
-    secondSegmentArrivalHours = secondSegmentdepartureTimestamp.getHours();
-    secondSegmentArrivalMinutes = secondSegmentdepartureTimestamp.getMinutes();
+    secondSegmentArrivalHours = String(
+      secondSegmentdepartureTimestamp.getHours()
+    ).padStart(2, "0");
+    secondSegmentArrivalMinutes = String(
+      secondSegmentdepartureTimestamp.getMinutes()
+    ).padStart(2, "0");
 
+    secondSegmentTravelHours = String(
+      Math.floor(props.data.tickets[0].segments[1].duration / 60)
+    ).padStart(2, "0");
+
+    secondSegmentTravelMinutes = String(
+      props.data.tickets[0].segments[1].duration -
+        Math.floor(props.data.tickets[0].segments[1].duration / 60) * 60
+    ).padStart(2, "0");
+
+    secondSegmentStops =
+      props.data.tickets[0].segments[1].stops.length === 0
+        ? "БЕЗ ПЕРЕСАДОК"
+        : props.data.tickets[0].segments[1].stops.length === 1
+        ? "1 ПЕРЕСАДКА"
+        : props.data.tickets[0].segments[1].stops.length > 1
+        ? `${props.data.tickets[0].segments[1].stops.length} ПЕРЕСАДКИ`
+        : "5 ПЕРЕСАДОК";
+
+        secondSegmentStopsNames = props.data.tickets[0].segments[1].stops.length > 1
+      ? props.data.tickets[0].segments[1].stops.join(", ")
+      : props.data.tickets[0].segments[1].stops.join("")
   }
   return (
-
     <>
       {props.data.tickets ? (
         <Box className={classes.card}>
           {console.log(props.data)}
           <Box className={classes.header}>
-            <Box>{price.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")} Р</Box>
+            <Box>{price} Р</Box>
             <Box>
               <img src={carrierImageUrl} alt="" />
             </Box>
@@ -123,76 +210,50 @@ function TicketCard(props) {
           <Box className={classes.destination}>
             <Box>
               <Box className={classes.destinationHeading}>
-                {props.data.tickets[0].segments[0].origin} –
-                {props.data.tickets[0].segments[0].destination}
+                {firstSegmentOrigin} –{firstSegmentDestination}
               </Box>
               <Box className={classes.destinationText}>
-                {firstSegmentDepartureHours}:{firstSegmentdepartureMinutes} – {firstSegmentArrivalHours}:
-                {firstSegmentArrivalMinutes}
+                {firstSegmentDepartureHours}:{firstSegmentdepartureMinutes} –{" "}
+                {firstSegmentArrivalHours}:{firstSegmentArrivalMinutes}
               </Box>
             </Box>
             <Box>
               <Box className={classes.destinationHeading}>В пути</Box>
               <Box className={classes.destinationText}>
-                {Math.floor(props.data.tickets[0].segments[0].duration / 60)}ч{" "}
-                {props.data.tickets[0].segments[0].duration -
-                  Math.floor(props.data.tickets[0].segments[0].duration / 60) *
-                    60}
-                м
+                {firstSegmentTravelHours}ч {firstSegmentTravelMinutes}м
               </Box>
             </Box>
             <Box>
               <Box className={classes.destinationHeading}>
-                {props.data.tickets[0].segments[0].stops.length === 0
-                  ? "БЕЗ ПЕРЕСАДОК"
-                  : props.data.tickets[0].segments[0].stops.length === 1
-                  ? "1 ПЕРЕСАДКА"
-                  : props.data.tickets[0].segments[0].stops.length > 1
-                  ? `${props.data.tickets[0].segments[0].stops.length} ПЕРЕСАДКИ`
-                  : "5 ПЕРЕСАДОК"}
+                {firstSegmentStops}
               </Box>
               <Box className={classes.destinationText}>
-                {props.data.tickets[0].segments[0].stops.length > 1
-                  ? props.data.tickets[0].segments[0].stops.join(", ")
-                  : props.data.tickets[0].segments[0].stops.join("")}
+                {firstSegmentStopsNames}
               </Box>
             </Box>
           </Box>
           <Box className={classes.destination}>
             <Box>
               <Box className={classes.destinationHeading}>
-                {props.data.tickets[0].segments[1].origin} –
-                {props.data.tickets[0].segments[1].destination}
+                {secondSegmentOrigin} –{secondSegmentDestination}
               </Box>
               <Box className={classes.destinationText}>
-                {secondSegmentDepartureHours}:{secondSegmentDepartureMinutes} – {secondSegmentArrivalHours}:
-                {secondSegmentArrivalMinutes}
+                {secondSegmentDepartureHours}:{secondSegmentDepartureMinutes} –{" "}
+                {secondSegmentArrivalHours}:{secondSegmentArrivalMinutes}
               </Box>
             </Box>
             <Box>
               <Box className={classes.destinationHeading}>В пути</Box>
               <Box className={classes.destinationText}>
-                {Math.floor(props.data.tickets[0].segments[1].duration / 60)}ч{" "}
-                {props.data.tickets[0].segments[1].duration -
-                  Math.floor(props.data.tickets[0].segments[1].duration / 60) *
-                    60}
-                м
+                {secondSegmentTravelHours}ч {secondSegmentTravelMinutes}м
               </Box>
             </Box>
             <Box>
               <Box className={classes.destinationHeading}>
-                {props.data.tickets[0].segments[1].stops.length === 0
-                  ? "БЕЗ ПЕРЕСАДОК"
-                  : props.data.tickets[0].segments[1].stops.length === 1
-                  ? "1 ПЕРЕСАДКА"
-                  : props.data.tickets[0].segments[1].stops.length > 1
-                  ? `${props.data.tickets[0].segments[1].stops.length} ПЕРЕСАДКИ`
-                  : "5 ПЕРЕСАДОК"}
+                {secondSegmentStops}
               </Box>
               <Box className={classes.destinationText}>
-                {props.data.tickets[0].segments[1].stops.length > 1
-                  ? props.data.tickets[0].segments[1].stops.join(", ")
-                  : props.data.tickets[0].segments[1].stops.join("")}
+                {secondSegmentStopsNames}
               </Box>
             </Box>
           </Box>

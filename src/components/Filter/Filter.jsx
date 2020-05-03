15b4c6c9c528e7cece72e417251a6026537d8058
Filipp Loss <filipploss@MacBook-Pro-Filipp.local>
2020-05-03 22:45:51 +0300

    filter logic added

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,36 +1,15 @@
 import React from "react";
-import Box from '@material-ui/core/Box';
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import clsx from "clsx";
 import Checkbox from "@material-ui/core/Checkbox";
 
-
-// import FormHelperText from "@material-ui/core/FormHelperText";
-
-// import "./Filter.css";
 import CheckboxTick from "../../images/Shape.svg";
 
-// export default function Filter() {
-//   return (
-//       <>
-//     {/* // <div className="filter-container"> */}
-//       {/* <p>КОЛИЧЕСТВО ПЕРЕСАДОК</p> */}
-//       <CheckboxesGroup />
-//     {/* </div> */}
-//     </>
-//   );
-// }
-
 const useStyles = makeStyles((theme) => ({
-  // root: {
-  //   "&:hover": {
-  //     backgroundColor: "transparent !important",
-  //   },
-  // },
 
   legend: {
     marginBottom: "10px",
@@ -48,25 +27,14 @@ const useStyles = makeStyles((theme) => ({
     fontStyle: "normal",
     fontWeight: "normal",
     fontSize: "13px",
-    // lineHeight: "20px",
-    // alignItems: "center",
     color: "#4A4A4A",
-
-    // display: flex;
-    // flexDirection: 'column',
     background: "#ffffff",
     boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
     borderRadius: "5px",
     width: "232px",
     minHeight: "252px",
-    cursor: 'url("./images/cursor.svg"), auto'
-    // "&:hover": {
-    //     backgroundColor: "transparent !important",
-    //   },
-    // padding: "20px",
-    // '&:hover': {
-    //     backgroundColor: 'blue',
-    //   },
+    cursor: 'url("./images/cursor.svg"), auto',
+   
   },
   icon: {
     borderRadius: "2px",
@@ -76,31 +44,9 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid #9ABBCE",
     boxSizing: "content-box",
     backgroundColor: "transparent",
-    // "&:hover": {
-    //     backgroundColor: "transparent !important",
-    //   },
-    // // "&:last-child": {
-    //     marginBottom: "20px",
-    //   },
-    // backgroundImage:
-    //   "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
-    // "$root.Mui-focusVisible &": {
-    //   outline: "2px auto rgba(19,124,189,.6)",
-    //   outlineOffset: 2,
-    // },
-    // "input:hover ~ &": {
-    //   backgroundColor: "transparent",
-    // },
-    // "input:disabled ~ &": {
-    //   boxShadow: "none",
-    //   background: "rgba(206,217,224,.5)",
-    // },
   },
   checkedIcon: {
-    //   padding: 0,
     border: "1px solid #2196F3",
-    // outline: '0px auto rgba(19,124,189,.6)',
-    // outlineOffset: 0,
     boxSizing: "content-box",
     borderRadius: "2px",
     backgroundColor: "transparent",
@@ -116,8 +62,8 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   formControl: {
-      flexGrow: 1,
-      lineHeight: "20px",
+    flexGrow: 1,
+    lineHeight: "20px",
   },
   formItem: {
     fontSize: "13px",
@@ -126,7 +72,6 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: "10px",
     "&:hover": {
       backgroundColor: "#F1FCFF",
-      // background: 'content-box'
     },
   },
 }));
@@ -155,6 +100,19 @@ export default function Filter() {
         all: false,
         [event.target.name]: event.target.checked,
       });
+    } else if (
+      state.all &&
+      !state.nonStop &&
+      !oneStop &&
+      !twoStops &&
+      !threeStops &&
+      event.target.name !== "all"
+    ) {
+      setState({
+        ...state,
+        all: false,
+        [event.target.name]: event.target.checked,
+      });
     } else {
       setState({ ...state, [event.target.name]: event.target.checked });
     }
@@ -172,17 +130,16 @@ export default function Filter() {
     } else {
       setState({
         all: true,
-        nonStop: true,
-        oneStop: true,
-        twoStops: true,
-        threeStops: true,
+        nonStop: false,
+        oneStop: false,
+        twoStops: false,
+        threeStops: false,
       });
     }
   };
 
   const { all, nonStop, oneStop, twoStops, threeStops } = state;
-  //   const error = [gilad, jason, antoine].filter((v) => v).length !== 2;
-  console.log('Filter loaded')
+  console.log("Filter loaded");
   return (
     <Box className={classes.filter}>
       <FormControl className={classes.formControl}>
@@ -203,7 +160,7 @@ export default function Filter() {
                 name="all"
                 checkedIcon={<span className={classes.checkedIcon} />}
                 icon={<span className={classes.icon} />}
-                disableRipple	
+                disableRipple
               />
             }
             label="Все"
@@ -217,7 +174,7 @@ export default function Filter() {
                 name="nonStop"
                 checkedIcon={<span className={classes.checkedIcon} />}
                 icon={<span className={classes.icon} />}
-                disableRipple	
+                disableRipple
               />
             }
             label="Без пересадок"
@@ -231,7 +188,7 @@ export default function Filter() {
                 name="oneStop"
                 checkedIcon={<span className={classes.checkedIcon} />}
                 icon={<span className={classes.icon} />}
-                disableRipple	
+                disableRipple
               />
             }
             label="1 пересадка"
@@ -245,7 +202,7 @@ export default function Filter() {
                 name="twoStops"
                 checkedIcon={<span className={classes.checkedIcon} />}
                 icon={<span className={classes.icon} />}
-                disableRipple	
+                disableRipple
               />
             }
             label="2 пересадки"
@@ -259,13 +216,12 @@ export default function Filter() {
                 name="threeStops"
                 checkedIcon={<span className={classes.checkedIcon} />}
                 icon={<span className={classes.icon} />}
-                disableRipple	
+                disableRipple
               />
             }
             label="3 пересадки"
           />
         </FormGroup>
-        {/* <FormHelperText>Be careful</FormHelperText> */}
       </FormControl>
     </Box>
   );
